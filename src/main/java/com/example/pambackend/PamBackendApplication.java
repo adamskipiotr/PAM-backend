e0781172a0dbe2bfa2dc527e8894dd2ee1939944
adamskipiotr <adamski-piotrek@wp.pl>
2020-10-20 01:09:59 +0200

    Init

---
+++ src/main/java/com/example/pambackend/PamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pambackend/PamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
